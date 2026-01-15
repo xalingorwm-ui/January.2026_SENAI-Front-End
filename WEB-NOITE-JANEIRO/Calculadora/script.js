98e0714b4b81866673e12830c548482828f92b85
Rafael Macedo <xalingorwm@gmail.com>
2026-01-14 21:22:37 -0300

    Update script.js

--- WEB-NOITE-JANEIRO/Calculadora/script.js
+++ WEB-NOITE-JANEIRO/Calculadora/script.js
@@ -1,1 +1,34 @@
+function soma(){
+    var numero1 = parseFloat(document.getElementById("n1").value)
+    var numero2 = parseFloat(document.getElementById("n2").value)
+    var resp = document.getElementById("resp")
+    // nao pode deixar console log nos codigos
+    // console.log(typeof numero1, typeof numero2)
+    resp.textContent = numero1 + numero2
+}
+
+function sub(){
+    var numero3 = parseFloat(document.getElementById("n3").value)
+    var numero4 = parseFloat(document.getElementById("n4").value)
+    var resp = document.getElementById("respSub")
+    resp.innerHTML = numero3 - numero4
+}
+
+function mult(){
+   var primeiro = parseFloat(document.getElementById("n5").value)
+   var segundo = parseFloat(document.getElementById("n6").value)
+   document.getElementById("respMult").innerHTML = primeiro * segundo
+}
+
+function div(){
+    var num1 = parseFloat(document.getElementById("n7").value)
+    var num2 = parseFloat(document.getElementById("n8").value)
+    
+    if(num2 !== 0){
+        document.getElementById("respDiv").innerHTML = (num1 / num2).toFixed(2)
+    }else{
+        document.getElementById("respDiv").innerHTML = "Não há divisão por 0, digite outro número!!!"
+        // alert("Não há divisão por 0, digite outro número!!!")
+    }
+}
 
